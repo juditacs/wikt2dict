d13cd0a7f3f240c9e66b64c8a44cb0b854ec5c79
Judit Acs <judit@sch.bme.hu>
2013-10-15 16:48:53 +0200

    Mode support for evaluating triangulation added

--- bin/compare_dicts.py
+++ bin/compare_dicts.py
@@ -5,10 +5,15 @@ from evaluator import Evaluator
 from handlers import ConfigHandler
 
 def main():
+    mode = argv[2]
     cfg = ConfigHandler('general', argv[1])
     e = Evaluator(cfg)
-    e.read_all_wiktionary()
-    e.compare_with_triangles_stdin()
+    if mode == 'wikt':
+        e.read_all_wiktionary()
+        e.compare_with_triangles_stdin()
+    elif mode == 'feat':
+        e.featurize_and_uniq_triangles_stdin()
+        e.write_overall('overall')
 
 if __name__ == '__main__':
     main()
