a0df6813376da99511bf2d140b1771698283693a
Judit Acs <judit@sch.bme.hu>
2014-02-20 12:59:41 +0100

    Started using docopt and a single central script with commands to make the user's life easier

---
+++ bin/w2d.py
@@ -0,0 +1,32 @@
+"""
+Wikt2Dict
+
+Usage:
+  w2d.py (download|textify) (--wikicodes=file|<wc>...)
+
+Options:
+  -h --help              Show this screen.
+  --version              Show version.
+  -w, --wikicodes=file   File containing a list of wikicodes.
+"""
+from docopt import docopt
+import logging
+
+logger = logging.getLogger('wikt2dict')
+
+def download_wiktionaries(fn=None, wc_list=None):
+    logger.info('Downloading Wiktionaries')
+    pass
+
+
+def main():
+    arguments = docopt(__doc__, version='Wikt2dict 1.1')
+    if arguments['download']:
+        if arguments['wikicodes']:
+            download_wiktionaries(fn=arguments['wikicodes'])
+        else:
+            download_wiktionaries(wc_list=arguments['wc'])
+    print(arguments)
+
+if __name__ == '__main__':
+    main()
