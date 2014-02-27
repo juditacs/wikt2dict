9f7ce09f3efce3ad8f6975d652f623fee3eb6355
Judit Acs <judit@sch.bme.hu>
2014-02-27 11:33:11 +0100

    Character unigram anomaly detection.
    	modified:   bin/find_anomalies.py

--- bin/find_anomalies.py
+++ bin/find_anomalies.py
@@ -2,26 +2,29 @@
 
 Usage:
     find_anomalies.py punct [--num=N]
-    find_anomalies.py unigram 
+    find_anomalies.py unigram <unigram_file> [--] <prob_threshold>
 
 Options:
     -n --num=N          Number of weird characters [default: 1].
     -h                  Show this screen.
 """
-from sys import stdin 
+from sys import stdin, stderr
 from docopt import docopt
 from collections import defaultdict
 import re
 import string
+import math
 
 punct_ok = set('-\'.')
 punct = ''.join(set(string.punctuation) - punct_ok)
 punct_re = re.compile(r'[{0}]'.format(re.escape(punct)),
                      re.UNICODE)
 
+unigrams = defaultdict(lambda: defaultdict(int))
+sum_ = defaultdict(int)
+
 def scan_stdin(args):
     stats = {'punct': 0, 'punct ok': 0, 'sum': 0, 'invalid': 0}
-    unigrams = defaultdict(lambda: defaultdict(int))
     for l in stdin:
         stats['sum'] += 1
         try:
@@ -33,26 +36,35 @@ def scan_stdin(args):
                 else:
                     stats['punct ok'] += 1
             if args['unigram']:
+                if not wc1 in sum_ or not wc2 in sum_:
+                    stderr.write('INVALID, unknown language: {0}'.format(l))
+                    continue
+                prob1 = 0.0
+                prob2 = 0.0
                 for c in w1:
-                    unigrams[wc1][c] += 1
+                    prob1 += math.log(float(unigrams[wc1][c]) / sum_[wc1])
+                prob1 /= len(w1)
                 for c in w2:
-                    unigrams[wc2][c] += 1
+                    prob2 += math.log(float(unigrams[wc2][c]) / sum_[wc2])
+                prob2 /= len(w2)
+                if prob1 < int(args['<prob_threshold>']) or prob2 < int(args['<prob_threshold>']):
+                    print('UNIGRAM: {0}\t{1}\t{2}'.format(prob1, prob2, l.strip()))
         except ValueError:
             stats['invalid'] += 1
-            #print('INVALID: {0}'.format(l.strip()))
-    #print(stats)
-    if args['unigram']:
-        print_unigrams(unigrams)
+            stderr.write('INVALID: {0}'.format(l))
 
 
-def print_unigrams(unigrams):
-    for wc, chars in unigrams.iteritems():
-        for c, cnt in sorted(((k, v) for k, v in chars.iteritems()), key=lambda x: -x[1]):
-            print(u'{0}\t{1}\t{2}'.format(wc, c, cnt).encode('utf8'))
-
+def read_unigrams(fn):
+    with open(fn) as f:
+        for l in f:
+            wc, c, cnt = l.decode('utf8').split('\t')
+            unigrams[wc][c] = int(cnt)
+            sum_[wc] += int(cnt)
 
 def main():
     args = docopt(__doc__, version="Wikt2Dict - Find anomalies 1.0")
+    if args['unigram']:
+        read_unigrams(args['<unigram_file>'])
     scan_stdin(args)
 
 if __name__ == '__main__':
