726b14cc9c2ba6f3ac5c3342e9d32254cbfd18e2
Judit Acs <judit@sch.bme.hu>
2014-02-28 14:13:30 +0100

    skeletons for config classes

---
+++ src/config.py
@@ -0,0 +1,16 @@
+class WiktionaryConfig(object):
+
+    full_name = ''
+    wc = ''
+    article_parsers = list()
+
+class ParserConfig(object):
+    pass
+
+
+class EsperantoConfig(WiktionaryConfig):
+
+    full_name = 'Esperanto'
+    wc = 'eo'
+
+
