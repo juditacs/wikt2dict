4069034ac0ee9011bb1b6ab73c20162d245d51b8
Judit Acs <judit@sch.bme.hu>
2014-03-07 11:27:24 +0100

    Both parsers work for Esperanto.
    	modified:   ../src/article_parsers.py
    	modified:   ../src/config.py

--- src/article_parsers.py
+++ src/article_parsers.py
@@ -89,24 +89,24 @@ class LangnamesArticleParser(ArticleParser):
     """ Class for parsing Wiktionaries that use simple lists for translations
     instead of templates """
 
-    def __init__(self, wikt, filter_langs=None):
-        ArticleParser.__init__(self, wikt, filter_langs)
+    def __init__(self, wikt_cfg, parser_cfg, filter_langs=None):
+        ArticleParser.__init__(self, wikt_cfg, parser_cfg, filter_langs)
         self.langname_field = int(self.cfg['language_name_field'])
         self.translation_field = int(self.cfg['translation_field'])
         self.translation_line_re = re.compile(ur'' + \
                    self.cfg['translation_line'].decode('utf8'), re.UNICODE)
         self.entity_delimiter = self.cfg['translation_entity_delimiter']
-        if self.cfg['skip_translation']:
-            self.skip_re_l = [i.decode('utf8') 
-                              for i in self.cfg['skip_translation'].split(',')]
-        else:
-            self.skip_re_l = None
-        self.read_langname_mapping(self.cfg)
+        #if self.cfg['skip_translation']:
+            #self.skip_re_l = [i.decode('utf8') 
+                              #for i in self.cfg['skip_translation'].split(',')]
+        #else:
+            #self.skip_re_l = None
+        self.read_langname_mapping()
 
-    def read_langname_mapping(self, cfg):
+    def read_langname_mapping(self):
         self.mapping = dict()
-        if cfg['uses_langnames'] == '1':
-            f = open(cfg['langnames'])
+        if self.cfg.langnames:
+            f = open(self.cfg.langnames)
             for l in f:
                 fields = l.strip().decode('utf8').split('\t')
                 for langname in fields[1:]:
@@ -115,7 +115,44 @@ class LangnamesArticleParser(ArticleParser):
                     self.mapping[langname.lower()] = fields[0]
             f.close()
         else:
-            self.mapping = dict([(wc, wc) for wc in self.wikicodes])
+            self.mapping = dict([(wc, wc) for wc in self.wikt_cfg.wikicodes])
+
+    def extract_translations(self, title, text):
+        translations = list()
+        for tr in self.translation_line_re.finditer(text):
+            if self.skip_translation_line(tr.group(0)):
+                continue
+            langname = tr.group(self.cfg.language_name_field).lower()
+            if not langname in self.mapping:
+                continue
+            wc = self.mapping[langname]
+            entities = self.get_entities(tr.group(self.cfg.translation_field))
+            for entity in entities:
+                entity_clear = self.trim_translation(entity)
+                if entity_clear:
+                    translations.append((wc, entity_clear))
+        return set(translations)
+
+    def trim_translation(self, word):
+        return word.replace('\n', ' ').strip()
+
+    def get_entities(self, trans_field):
+        trimmed = self.cfg.bracket_re.sub('', trans_field)
+        entities = list()
+        for e in self.cfg.delimiter_re.split(trimmed):
+            for m in self.cfg.translation_re.finditer(e):
+                word = m.group(1)
+                if self.skip_entity(word):
+                    continue
+                entities.append(word)
+        return set(entities)
+
+    def skip_entity(self, entity):
+        if self.cfg.skip_translation_re.search(entity):
+            return True
+        if self.cfg.junk_re.search(entity):
+            return True
+        return False
 
     def get_pairs(self, text):
         translations = defaultdict(list)
@@ -136,26 +173,26 @@ class LangnamesArticleParser(ArticleParser):
                         translations[wc].append(ent)
         return translations
 
-    def trim_translation(self, trans, wc=None):
-        """ take wc as parameters to make it possible to parse based on language """
-        if wc == 'ja' and '{' in trans:
-            fd = re.search('\{\{([^}]*)\}\}', trans, re.UNICODE)
-            if fd:
-                try:
-                    return fd.group(1).split('|')[1]
-                except IndexError:
-                    self.wiktionary.log_handler.error("IndexError in entity " + trans.encode('utf8'))
+    #def trim_translation(self, trans, wc=None):
+        #""" take wc as parameters to make it possible to parse based on language """
+        #if wc == 'ja' and '{' in trans:
+            #fd = re.search('\{\{([^}]*)\}\}', trans, re.UNICODE)
+            #if fd:
+                #try:
+                    #return fd.group(1).split('|')[1]
+                #except IndexError:
+                    #self.wiktionary.log_handler.error("IndexError in entity " + trans.encode('utf8'))
         #TODO fix [[kanojo]] [kanojo]
-        trans = re.sub('\'\'+[^\']*\'+', '', trans)
-        trans = re.sub('""+[^"]*"+', '', trans)
-        trans = re.sub('\{\{[^}]*\}\}', '', trans)
-        trans = trans.replace('[', '')
-        trans = trans.replace(']', '')
-        trans = trans.replace('{', '')
-        trans = trans.replace('}', '')
-        trans = trans.split('|')[0]
-        trans = trans.split('/')[0]
-        return trans.strip()
+        #trans = re.sub('\'\'+[^\']*\'+', '', trans)
+        #trans = re.sub('""+[^"]*"+', '', trans)
+        #trans = re.sub('\{\{[^}]*\}\}', '', trans)
+        #trans = trans.replace('[', '')
+        #trans = trans.replace(']', '')
+        #trans = trans.replace('{', '')
+        #trans = trans.replace('}', '')
+        #trans = trans.split('|')[0]
+        #trans = trans.split('/')[0]
+        #return trans.strip()
 
 
 
@@ -178,8 +215,7 @@ class DefaultArticleParser(ArticleParser):
 
     def extract_translations(self, title, text):
         translations = list()
-        m = self.trad_re.finditer(text)
-        for tr in m:
+        for tr in self.trad_re.finditer(text):
             wc = tr.group(self.cfg.wc_field)
             if not wc.strip() or not wc in self.wikt_cfg.wikicodes:
                 continue
--- src/config.py
+++ src/config.py
@@ -9,16 +9,33 @@ wiktionary_defaults = {
 }
 
 parser_defaults = {
-    'translation_prefix': ur't[\u00f8\+\-]?',
-    'wc_field': 1,
-    'word_field': 2,
     'blacklist': ['PAGENAME'],  # words that should not appear
 }
 
-langname_parser_defaults = {
+default_parser_defaults = {
+    'translation_prefix': ur't[\u00f8\+\-]?',
+    'wc_field': 1,
+    'word_field': 2,
 }
 
-class WiktionaryConfig(object):
+langname_parser_defaults = {
+    'language_name_field': 1,
+    'translation_field': 2,
+    'translation_entity_delimiter': ',',
+    'translation_re': re.compile(r'\[\[([^\[\]]+)\]\]', re.UNICODE)
+}
+
+
+class DictLikeClass(object):
+
+    def __getitem__(self, key):
+        return self.__dict__.get(key, None)
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+
+class WiktionaryConfig(DictLikeClass):
 
     def __init__(self):
         for key, value in wiktionary_defaults.iteritems():
@@ -27,12 +44,6 @@ class WiktionaryConfig(object):
         self.wc = ''
         self.parsers = list()
         self._wikicodes = None
-
-    def __getitem__(self, key):
-        return self.__dict__.get(key, None)
-    
-    def __setitem__(self, key, value):
-        self.__dict__[key] = value
 
     @property
     def wikicodes(self):
@@ -43,14 +54,19 @@ class WiktionaryConfig(object):
 
     @property
     def dump_filename(self):
-        return self.wc + self.dump_file_postfix 
+        return self.wc + self.dump_file_postfix
+
+    @property
+    def dump_path(self):
+        return path.join(self.dump_path_base, self.full_name, self.dump_filename)
 
 
-class ParserConfig(object):
+class ParserConfig(DictLikeClass):
 
-    defaults = {
-    }
+    defaults = parser_defaults
+
     def __init__(self, wikt_cfg=None, parser_cfg=None):
+        self._skip_trans_re = None
         for key, value in self.defaults.iteritems():
             self[key] = value
         if parser_cfg:
@@ -59,6 +75,13 @@ class ParserConfig(object):
         if wikt_cfg:
             for key, value in wikt_cfg.iteritems():
                 self[key] = value
+
+    @property
+    def skip_translation_re(self):
+        if not self._skip_trans_re:
+            self._skip_trans_re = re.compile(
+                ur'(' + '|'.join(self.blacklist) + ')', re.UNICODE)
+        return self._skip_trans_re
 
     def __setitem__(self, key, value):
         self.__dict__[key] = value
@@ -70,26 +93,18 @@ class ParserConfig(object):
 class DefaultParserConfig(ParserConfig):
 
     def __init__(self, wikt_cfg=None, parser_cfg=None):
-        self.defaults.update(parser_defaults)
+        self.defaults.update(default_parser_defaults)
         self._trad_re = None
-        self._skip_trans_re = None
+        self.features = ['defaultparser']
         super(DefaultParserConfig, self).__init__(wikt_cfg, parser_cfg)
-
-    @property
-    def skip_translation_re(self):
-        if not self._skip_trans_re:
-            self._skip_trans_re = re.compile(
-                ur'(' + '|'.join(self.blacklist) + ')', re.UNICODE)
-        return self._skip_trans_re
 
     @property
     def trad_re(self):
         if not self._trad_re:
-            re_str = (r'\{\{' + self.translation_prefix +
-                      r'\|([^}|]+)\|'  # wikicode 
-                      r'([^}|]*)'  # word
-                      r'(\|[^}]*)*\}\}')  # rest
-            self._trad_re = re.compile(re_str, re.UNICODE)
+            self._trad_re = re.compile(r'\{\{' + self.translation_prefix +
+                                       r'\|([^}|]+)\|'  # wikicode
+                                       r'([^}|]*)'  # word
+                                       r'(\|[^}]*)*\}\}', re.UNICODE)  # rest
         return self._trad_re
 
 
@@ -97,6 +112,9 @@ class LangnamesParserConfig(ParserConfig):
 
     def __init__(self, wikt_cfg=None, parser_cfg=None):
         self.defaults.update(langname_parser_defaults)
+        self._bracket_re = None
+        self._delimiter_re = None
+        self.features = ['langnames_parser']
         super(LangnamesParserConfig, self).__init__(wikt_cfg, parser_cfg)
 
 
