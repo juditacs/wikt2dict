2a97073db13e3f7c3bd9f2362767940aea248ea2
Judit Acs <judit@sch.bme.hu>
2014-03-19 10:03:45 +0100

    Adding triangulating support. 	modified:   w2d.py

--- bin/w2d.py
+++ bin/w2d.py
@@ -10,9 +10,12 @@ Options:
   -w, --wikicodes=file   File containing a list of wikicodes.
 """
 from docopt import docopt
+from sys import stderr
+from itertools import combinations
 import logging
 
 from wiktionary import Wiktionary
+from triangulator import Triangulator
 
 logger = logging.getLogger('wikt2dict')
 
@@ -35,8 +38,26 @@ def extract_translations(wc_list=None, fn=None):
         wikt.parse_articles()
 
 
+def triangulate(wc_set):
+    n = 53
+    num_of_tr = n * (n - 1) * (n - 2) / 6
+    i = 1
+    for triangle_wc in combinations(wc_set, 3):
+        stderr.write(str(i) + '/' + str(num_of_tr) + repr(triangle_wc) + '\n')
+        i += 1
+        logger.info(' '.join(triangle_wc) + ' triangle')
+        triangulator = Triangulator(triangle_wc)
+        triangulator.collect_triangles()
+        triangulator.write_triangles()
+
+
 def main():
     arguments = docopt(__doc__, version='Wikt2dict 1.1')
+    if arguments['--wikicodes']:
+        with open(arguments['--wikicodes']) as f:
+            wc_set = set([l.strip() for l in f])
+    else:
+        wc_set = set(arguments['<wc>'])
     if arguments['download']:
         if arguments['--wikicodes']:
             download_wiktionaries(fn=arguments['--wikicodes'])
@@ -47,6 +68,8 @@ def main():
             extract_translations(fn=arguments['--wikicodes'])
         else:
             extract_translations(wc_list=arguments['<wc>'])
+    if arguments['triangulate']:
+        triangulate(wc_set)
 
 if __name__ == '__main__':
     main()
