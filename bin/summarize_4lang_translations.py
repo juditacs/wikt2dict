85b08147eface093fbc5f66f28378e33097c661a
Judit Acs <judit@sch.bme.hu>
2014-03-19 10:08:14 +0100

    Summarize 4lang translations. 	new file:   summarize_4lang_translations.py

---
+++ bin/summarize_4lang_translations.py
@@ -0,0 +1,30 @@
+from sys import argv, stdin
+from collections import defaultdict
+import json
+import math
+
+
+def main():
+    stats = defaultdict(list)
+    topN = int(argv[2])
+    for l in stdin:
+        fd = l.decode('utf8').split('\t')
+        trans = json.loads(fd[1])
+        for wc, words in trans.iteritems():
+            stats[wc].append(len(words))
+    s = 0
+    fourlang_len = 3478.0
+    for wc, stat in sorted(stats.iteritems(), key=lambda x: -len(x[1]))[0:topN]:
+        s += len(stat)
+        print('{} {}'.format(wc, len(stat) / fourlang_len))
+    E = s / float(topN)
+    var = 0.0
+    for wc, stat in sorted(stats.iteritems(), key=lambda x: -len(x[1]))[0:topN]:
+        var += (len(stat) - E) ** 2
+    print E
+    print math.sqrt(var / float(topN))
+    print s / float(topN)
+
+
+if __name__ == '__main__':
+    main()
