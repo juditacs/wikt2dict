eb795c4441fd6e0b2bc4931dfc29d77800029bcf
Judit Acs <judit@sch.bme.hu>
2014-03-31 14:14:25 +0200

    simple setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    author='Judit Acs',
+    author_email='judit@sch.bme.hu',
+    name='wikt2dict',
+    provides=['wikt2dict'],
+    url='https://github.com/juditacs/wikt2dict',
+    packages=['wikt2dict'],
+    package_dir={'': '.'},
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['docopt'],
+)
